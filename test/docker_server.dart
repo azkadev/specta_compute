4fb8355f8dac6cde174ca0d26266928c0c74c5d3
Your Name <you@example.com>
2022-10-13 11:48:38 +0700

    updtae

--- test/docker_server.dart
+++ test/docker_server.dart
@@ -1,17 +1,18 @@
 import 'package:docker_commander/docker_commander_vm.dart';
 
 void main() async {
-  
   // A simple username and password table:
   var authenticationTable = AuthenticationTable({'admin': '123'});
 
   // A `DockerHost` Server at port 8099:
   var hostServer = DockerHostServer(
-          (user, pass) async => authenticationTable.checkPassword(user, pass),
-      8099);
+    (user, pass) async {
+      return authenticationTable.checkPassword(user, pass);
+    },
+    8099,
+  );
 
   // Starts the server and wait initialization:
   await hostServer.startAndWait();
   print("server run");
-  
 }
