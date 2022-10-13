345a782a4d9d8265696e494dff42d0fecc59aed2
Your Name <you@example.com>
2022-10-13 14:57:44 +0700

    update

--- bin/specta_paas.dart
+++ bin/specta_paas.dart
@@ -1,13 +1,12 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:specta_paas/specta_paas.dart' as specta_paas;
 import 'package:telegram_client/telegram_client.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${specta_paas.calculate()}!');
-  TelegramBotApi tg = TelegramBotApi("");
-  int chat_id = 0;
-  Timer.periodic(Duration(seconds: 2), (t) async {
-    await tg.request("sendMessage", parameters: {"chat_id": chat_id, "text": t.toString()});
-  });
+  var res = Process.runSync("ls", [""]);
+  print(res.stderr);
+  print(res.stdout);
+  print(Platform.environment["azka"]);
 }
--- test/docker_local.dart
+++ test/docker_local.dart
@@ -1,23 +1,117 @@
+// ignore_for_file: unnecessary_string_interpolations
+
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:docker_commander/docker_commander_vm.dart';
 
 void main() async {
-  // Creates a `DockerCommander` for a local host machine:
-  var dockerCommander = DockerCommander(DockerHostLocal());
-  
-  // Initialize `DockerCommander`:
-  await dockerCommander.initialize();
-  // Ensure that Docker daemon is running.
-  await dockerCommander.checkDaemon();
+  DockerCli dockerCli = DockerCli();
+  await dockerCli.init();
+  DockerCommander docker = dockerCli.docker;
 
-  // Run Docker image `hello-world`:
-  var dockerContainer = await dockerCommander.run('azka');
+  // Process build = await Process.start(
+  //   "docker",
+  //   ["build", "https://github.com/azkadev/specta_userbot_telegram.git#main", "-t", "azkadev/userbot"],
+  //   runInShell: true,
+  // );
+  // print(await build.exitCode);
+  // build.stderr.listen((event) {
+  //   print(utf8.decode(event));
+  // });
+  // build.stdout.listen((event) {
+  //   print(utf8.decode(event));
+  // });
 
-  // Waits the container to exit, and gets the exit code:
-  var exitCode = await dockerContainer!.waitExit();
+  var buid = await dockerCli.buildImage(
+    nameImage: "azkadev/userbot",
+    dockerImage:DockerImage.fromDirectory(directory: Directory("../")),
+    onData: (dockerProcess) {
+      print(dockerProcess.stderr!.asString);
+    },
+  );
+  var res = await docker.command("ps", []);
+  print(res!.stdout!.asString);
+  // var getall = await docker.psContainerNames();
+  // getall ??= [];
+  // if (getall.length > 1) {
+  //   print(getall);
+  //   print("delete");
+  //   await docker.stopContainer(getall.first);
+  //   var getalls = await docker.psContainerNames();
+  //   print(getalls);
+  //   return;
+  // }
   
-  // Gets all the STDOUT as [String]. 
-  var output = dockerContainer.stdout!.asString;
-  
+  var dockerContainer = await docker.run('azkadev/userbot', containerName: "azkacantip"); 
+  // Gets all the STDOUT as [String].
+  var output = dockerContainer!.stdout!.asString;
+
   print(output);
-  print('EXIT CODE: $exitCode');
+}
+
+class DockerCli {
+  late DockerCommander docker;
+  DockerCli({DockerCommander? dockerCommander}) {
+    dockerCommander ??= DockerCommander(DockerHostLocal());
+    docker = dockerCommander;
+  }
+
+  Future<void> init() async {
+    await docker.initialize();
+    await docker.checkDaemon();
+  }
+
+  Future<DockerProcess?> buildImage({
+    required String nameImage,
+    required DockerImage dockerImage,
+    void Function(DockerProcess dockerProcess)? onData,
+  }) async {
+    DockerProcess? re = await docker.command(
+      "build",
+      [dockerImage.data, "-t", nameImage, "--progress=plain"],
+    );
+
+    if (re != null) {
+      return await Future.microtask(() async {
+        while (true) {
+          await Future.delayed(Duration(milliseconds: 10));
+          if (re.isFinished) {
+            return re;
+          } else {
+            if (onData != null) {
+              onData.call(re);
+            }
+          }
+        }
+      });
+    }
+  }
+
+  Future<List> getAllImages() async {
+    return await docker.psContainerNames() ?? [];
+  }
+
+  Future<DockerProcess?> remove({required String imageOrId}) async  {
+    return await docker.command("rmi", [imageOrId]);
+  }
+  Future<DockerProcess?> removeAll() async  {
+    return await docker.command("rmi", ["\$(docker images -q)", "--force"]);
+  }
+}
+
+class DockerImage {
+  late String data;
+  DockerImage(this.data);
+  factory DockerImage.fromGit({
+    required String url,
+    String branch = "master",
+  }) {
+    return DockerImage("${url}#${branch.toString()}");
+  }
+  factory DockerImage.fromDirectory({
+    required Directory directory,
+  }) {
+    return DockerImage("${directory.path}");
+  }
 }
