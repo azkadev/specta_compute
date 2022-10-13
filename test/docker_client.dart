ebdeda13bab8ed7a1cc27d0be07da5815edd0537
Azka Full Snack Developer:) <82513502+azkadev@users.noreply.github.com>
2022-10-13 06:05:27 +0000

    update

--- test/docker_client.dart
+++ test/docker_client.dart
@@ -2,7 +2,7 @@ import 'package:docker_commander/docker_commander.dart';
 
 void main() async {
   // Connect to a `DockerHost` running at '10.0.0.52:8099'
-  var dockerHostRemote = DockerHostRemote('azkadev-specta-paas-9xr7prv9p96c7pj4-8099.githubpreview.dev', 443, secure: true, username: 'admin', password: '123');
+  var dockerHostRemote = DockerHostRemote('127.0.0.1', 8099, secure: false, username: 'admin', password: '123');
 
   // Creates a `DockerCommander` for a remote host machine:
   var dockerCommander = DockerCommander(dockerHostRemote);
@@ -12,6 +12,17 @@ void main() async {
   // Ensure that Docker daemon is running (at remote server):
   await dockerCommander.checkDaemon();
 
+  var getall = await dockerCommander.psContainerNames();
+  getall ??= [];
+  if (getall.length > 1) {
+    print(getall);
+    print("delete");
+    await dockerCommander.stopContainer(getall.first); 
+  var getalls = await dockerCommander.psContainerNames();
+  print(getalls);
+    return;
+  }
+  print(getall);
   // Run Docker image `hello-world` (at remote server):
   var dockerContainer = await dockerCommander.run('azka');
 
@@ -23,5 +34,8 @@ void main() async {
   // Gets all the STDOUT as [String].
   var output = dockerContainer!.stdout!.asString;
   print(output);
+
+  var get_all = await dockerCommander.psContainerNames();
+  print(get_all);
   // ...
 }
