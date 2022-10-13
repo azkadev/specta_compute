e91fb615bbef86d710355633bc7fa31e7b9fcafd
Azka Full Snack Developer:) <82513502+azkadev@users.noreply.github.com>
2022-10-13 04:37:29 +0000

    update

---
+++ test/docker_client.dart
@@ -0,0 +1,30 @@
+import 'package:docker_commander/docker_commander.dart';
+
+void main() async {
+
+  // Connect to a `DockerHost` running at '10.0.0.52:8099'
+  var dockerHostRemote = DockerHostRemote('10.0.0.52', 8099, username: 'admin', password: '123');
+
+  // Creates a `DockerCommander` for a remote host machine:
+  var dockerCommander = DockerCommander(dockerHostRemote);
+  
+  // Initialize `DockerCommander` (at remote server):
+  await dockerCommander.initialize();
+  // Ensure that Docker daemon is running (at remote server):
+  await dockerCommander.checkDaemon();
+
+  // Run Docker image `hello-world` (at remote server):
+  var dockerContainer = await dockerCommander.run('hello-world');
+
+  // The behavior is the same of the example using `DockerHostLocal`.
+  // The internal `DockerRunner` will sync remote output (stdout/sdterr) automatically!
+
+  // ...
+  
+  // Gets all the STDOUT as [String]. 
+  var output = dockerContainer.stdout.asString;
+  print(output);
+  
+  // ...
+  
+}
