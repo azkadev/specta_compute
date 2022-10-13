b2d2cc57fc660c0221807b18a3d7d3d8cd7411e9
Your Name <you@example.com>
2022-10-13 11:10:43 +0700

    update

---
+++ bin/specta_paas.dart
@@ -0,0 +1,5 @@
+import 'package:specta_paas/specta_paas.dart' as specta_paas;
+
+void main(List<String> arguments) {
+  print('Hello world: ${specta_paas.calculate()}!');
+}
---
+++ test/specta_paas_test.dart
@@ -0,0 +1,8 @@
+import 'package:specta_paas/specta_paas.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
