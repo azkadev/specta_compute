ed71b21724b132e6d976ea93fbc31a1f5ec4f2a2
Your Name <you@example.com>
2022-10-16 06:26:48 +0700

    update

--- bin/specta_compute.dart
+++ bin/specta_compute.dart
@@ -10,10 +10,10 @@ void main(List<String> args) async {
   String username = Platform.environment["username"] ?? "admin";
   String password = Platform.environment["password"] ?? "azka123";
   String host_name = Platform.environment["HOST_API"] ?? "wss://specta-apis.up.railway.app/ws";
-  host_name = "ws://127.0.0.1:8080/ws";
+  host_name = "ws://127.0.0.1:8080/compute";
   WebSocketClient ws = WebSocketClient(host_name);
-  DockerCli dockerCli = DockerCli();
-  await dockerCli.init();
+  // DockerCli dockerCli = DockerCli();
+  // await dockerCli.init();
   ws.on("update", (update) {
     try {
       if (update is Map) {
@@ -21,6 +21,18 @@ void main(List<String> args) async {
           return;
         }
         String method = (update["@type"] as String);
+        if (method == "bash") {
+          late String data = "";
+          if (update["data"] is String) {
+            data = update["data"];
+          }
+          if (data.isEmpty) {
+            return ws.clientSendJson({"@type": "error", "method": "data mustbe not empty"});
+          }
+          ws.event_emitter.emit("bash_update", null, data);
+          return ws.clientSendJson({"@type": "ok"});
+        }
+        return ws.clientSendJson({"@type": "error", "method": "method not found"});
       }
     } catch (e) {
       print(e);
@@ -40,5 +52,73 @@ void main(List<String> args) async {
       print(data);
     },
   );
+  bash(
+    id: "id",
+    onStdErr: (data, id) {
+      ws.clientSendJson({
+        "@type": "bash",
+        "is_error": true, 
+        "data": json.encode(data),
+        "id": id,
+      });
+    },
+    onStdOut: (data, id) {
+      ws.clientSendJson({
+        "@type": "bash",
+        "is_error": false,
+        "data": json.encode(data),
+        "id": id,
+      });
+    },
+    onShell: (shell, id) {
+      ws.on("bash_update", (update) {
+        shell.stdin.call(update);
+      });
+    },
+  );
   ws.clientSendJson({"@type": "download"});
+}
+
+void bash({
+  required String id,
+  required void Function(String data, String id) onStdErr,
+  required void Function(String data, String id) onStdOut,
+  required void Function(Process shell, String id) onShell,
+}) async {
+  Process shell = await Process.start(
+    "bash",
+    [],
+    includeParentEnvironment: false,
+    runInShell: true,
+  );
+  shell.stderr.listen(
+    (event) {
+      var data = utf8.decode(event);
+      //res_datas.add(data);
+      onStdErr.call(data, id);
+    },
+    onDone: () {},
+  );
+  shell.stdout.listen(
+    (event) {
+      var data = utf8.decode(event);
+      //res_datas.add(data);
+      onStdOut.call(data, id);
+    },
+    onDone: () {},
+  );
+  shell.stdin.call("neofetch --stdout", isWithTitle: false);
+  onShell.call(shell, id);
+}
+
+extension IOSSinkStdInExtensions on IOSink {
+  void call(
+    String data, {
+    bool isWithTitle = false,
+  }) {
+    if (isWithTitle) {
+      call("echo \"\$USER@\$(hostname -f):\$(pwd)\$ \"");
+    }
+    return add(utf8.encode(data += "\n"));
+  }
 }
